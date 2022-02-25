dac5648c8c81cd24ca6b30cae121697615c50caf
Vsux <vsux17@gmail.com>
2022-02-25 09:28:37 -0200

    form, pag. login e sing up

---
+++ modulo3/Cookenu/cookenu/src/pages/LoginPage/LoginForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { InputsContainer,  ScreenContainer,  } from "./styled";
+import { TextField } from "@material-ui/core";
+import { useForm } from "../../hooks/useForm";
+import Button from "@mui/material/Button";
+export const LoginForm = () => {
+    const [form, onchange, clear] = useForm({ email: "", password: "" })
+    const onSubmitForm = (event) => {
+        event.preventDefaut()
+    }
+    return (
+
+        <ScreenContainer>
+           
+            <InputsContainer>
+                <form onSubmit={onSubmitForm} >
+                    <TextField
+                        name={"email"}
+                        value={form.email}
+                        onChange={onchange}
+                        label={"Email"}
+                        variant={"outlined"}
+                        fullWidth
+                        margin="dense"
+                        required
+                        type={"email"}
+                    />
+                    <TextField
+                        name={"password"}
+                        value={form.password}
+                        onChange={onchange}
+                        label={"Senha"}
+                        variant={"outlined"}
+                        fullWidth
+                        margin="dense"
+                        required
+                        type={"password"}
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant={"contained"}
+                        color={"primary"}
+
+                    >Fazer login
+                    </Button>
+
+
+                </form>
+            </InputsContainer>
+          
+         
+        </ScreenContainer>
+
+
+    )
+
+}
--- modulo3/Cookenu/cookenu/src/pages/LoginPage/LoginPage.js
+++ modulo3/Cookenu/cookenu/src/pages/LoginPage/LoginPage.js
@@ -1,55 +1,36 @@
 import React from "react";
-import { InputsContainer, LogoImage, ScreenContainer } from "./styled";
+import {  LogoImage, ScreenContainer, SignUpButtonContainer } from "./styled";
 import logo from "../../assets/logo.png"
-import {  TextField } from "@material-ui/core";
 import { useForm } from "../../hooks/useForm";
-import  Button  from "@mui/material/Button";
-export const LoginPage = () => {
-    const [form, onchange, clear] = useForm({ email: "", password: "" })
-    const onSubmitForm = () => {
+import Button from "@mui/material/Button";
+import { LoginForm } from "./LoginForm";
+import { goSignUpPage } from "../../routes/coordinator";
+import { useNavigate } from "react-router-dom";
 
-    }
+
+
+
+
+
+export const LoginPage = () => {
+    const navigate= useNavigate()
     return (
 
         <ScreenContainer>
             <LogoImage src={logo} />
-            <InputsContainer>
-                <form onSubmit={onSubmitForm} >
-                    <TextField
-                        name={"email"}
-                        value={form.email}
-                        onChange={onchange}
-                        label={"Email"}
-                        variant={"outlined"}
-                        fullWidth
-                        margin="dense"
-                        required
-                        type={"email"}
-                    />
-                    <TextField
-                        name={"password"}
-                        value={form.password}
-                        onChange={onchange}
-                        label={"Senha"}
-                        variant={"outlined"}
-                        fullWidth
-                        margin="dense"
-                        required
-                        type={"password"}
-                    />
-
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant={"contained"}
-                        color={"primary"}
-                        
-                    >Fazer login
-                    </Button>
-
-
-                </form>
-            </InputsContainer>
+            <LoginForm/>
+               
+           <SignUpButtonContainer>
+                <Button
+                onClick={()=> goSignUpPage(navigate) }
+                    type="submit"
+                    fullWidth
+                    variant="text"
+                    color={"primary"}
+    
+                >Não possui conta? Cadastre-se agora
+                </Button>
+           </SignUpButtonContainer>
         </ScreenContainer>
 
 
