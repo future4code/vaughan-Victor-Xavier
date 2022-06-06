01a8c4ea386c67340205d216b497cb1bf30452c0
Vsux <vsux17@gmail.com>
2022-06-06 12:20:31 -0200

    base backend

---
+++ modulo6/case4-doghero/src/index.ts
@@ -0,0 +1,22 @@
+import express, {Express} from 'express'
+import cors from 'cors'
+import { AddressInfo } from 'net';
+import dotenv from "dotenv";
+const app: Express = express();
+
+dotenv.config();
+
+
+app.use(express.json());
+app.use(cors())
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
